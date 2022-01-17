50ea275957a33e4fb30ddde49e44c9e94241a587
Erick Santos <erickhenrique321@gmail.com>
2022-01-17 18:14:12 -0300

    remove login

--- apps/dashboard/webpack.prod.config.js
+++ apps/dashboard/webpack.prod.config.js
@@ -40,7 +40,7 @@ module.exports = {
   plugins: [
     new ModuleFederationPlugin({
       remotes: {
-        login: 'login@https://hardcore-poitras-f94c02.netlify.app/',
+        login: 'https://hardcore-poitras-f94c02.netlify.app/',
       },
       shared: {
         '@angular/core': { singleton: true, strictVersion: true },
